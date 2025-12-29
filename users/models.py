207b440fd8fd10096a95e838bd92c686e497b939
xziste <17alexdmz99@gmail.com>
2025-12-29 21:05:52 +0300

    added models in app users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    phone = models.CharField(max_length=20)
+    birth_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
