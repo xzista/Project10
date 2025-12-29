6d823ca011e031b502b4580008a68f71cc9f2975
xziste <17alexdmz99@gmail.com>
2025-12-29 22:42:39 +0300

    added models in app posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    image = models.ImageField(upload_to="posts/", null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
