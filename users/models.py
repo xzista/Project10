647d12cb882e5426262cbbb7da062373d1398ac7
xziste <17alexdmz99@gmail.com>
2025-12-30 08:10:30 +0300

    update models in app users

--- users/models.py
+++ users/models.py
@@ -2,7 +2,19 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    """
+    Кастомный пользователь с расширенными полями профиля (телефон и дата рождения).
+    """
+
     phone = models.CharField(max_length=20)
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ("-created_at",)
+
+    def __str__(self):
+        return self.username
