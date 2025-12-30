d57cedef2bf0a4d14f63bee6c0b2c7fc4afb13d4
xziste <17alexdmz99@gmail.com>
2025-12-30 09:37:10 +0300

    added permissions.py in posts

---
+++ posts/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAdminOrOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        if request.user.is_staff:
+            return True
+        return obj.author == request.user
