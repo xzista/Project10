9089329746a56895343e0fae6e4f739c2ad7322a
xziste <17alexdmz99@gmail.com>
2026-01-03 10:20:24 +0300

    added all urls in project

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet, basename="user")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
