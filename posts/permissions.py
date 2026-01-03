5b9ef8fd6df7fb5a89c95e8acabfb9e735d8cd01
xziste <17alexdmz99@gmail.com>
2026-01-03 09:51:30 +0300

    update permissions.py in posts

--- posts/permissions.py
+++ posts/permissions.py
@@ -1,9 +1,8 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 class IsAdminOrOwner(BasePermission):
+    """
+    Разрешает доступ, если является администратором или автором (владельцем)
+    """
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        if request.user.is_staff:
-            return True
-        return obj.author == request.user
+        return request.user.is_staff or obj.author == request.user
