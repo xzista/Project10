471637fcd85b30b33ef207806dda6a10de4353f4
xziste <17alexdmz99@gmail.com>
2026-01-03 16:14:06 +0300

    added python-dateutil in project and updated validate_author_age in services.py posts

--- posts/services.py
+++ posts/services.py
@@ -1,12 +1,13 @@
 from datetime import date
 from django.core.exceptions import ValidationError
+from dateutil.relativedelta import relativedelta
 
 FORBIDDEN_WORDS = ("ерунда", "глупость", "чепуха")
 
 def validate_author_age(user):
     if not user.birth_date:
         raise ValidationError("Дата рождения не указана.")
-    age = (date.today() - user.birth_date).days // 365
+    age = relativedelta(date.today(), user.birth_date).years
     if age < 18:
         raise ValidationError("Автору должно быть не менее 18 лет.")
 
