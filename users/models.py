d2556426fb67eaa4fbb200ffac1238cac89b4a34
xziste <17alexdmz99@gmail.com>
2026-01-03 16:12:30 +0300

    updated models.py in users

--- users/models.py
+++ users/models.py
@@ -6,10 +6,20 @@ class User(AbstractUser):
     Кастомный пользователь с расширенными полями профиля (телефон и дата рождения).
     """
 
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        null=False,
+        error_messages={
+            'unique': "Пользователь с таким email уже существует.",
+        },
+    )
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ["email"]
 
     class Meta:
         verbose_name = "Пользователь"
